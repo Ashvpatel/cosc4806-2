b18ffa6724447217f0daa6872b96663f4d837ae4
Ashvpatel <ashvpatel@algomau.ca>
2024-06-01 03:19:06 +0000

    Wrote an html code which create register form for new user
    where users can input a username and password, with password validation requiring a minimum length of 8 characters.

--- register.php
+++ register.php
@@ -14,3 +14,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 ?>
+
+< !DOCTYPE html >
+    <html>
+    <head>
+        <title>Register</title>
+    </head>
+    <body>
+        <form method="post">
+            Username: <input type="text" name="username" required><br>
+            Password: <input type="password" name="password" required minlength="8"><br>
+            <button type="submit">Create an account</button>
+        </form>
+        <p>Password must be at least 8 characters long.</p>
+    </body>
+    </html>
