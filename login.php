b150a94015e8f6a002c78e95b15140e116e7713d
Ashvpatel <ashvpatel@algomau.ca>
2024-06-01 03:22:17 +0000

    Copied login form from cosc4806-1 assignment 1 which create a login form for user

---
+++ login.php
@@ -0,0 +1,33 @@
+<?php
+
+session_start();
+if (isset($_SESSION['authenticated']) && $_SESSION['authenticated'] == 1) {
+    header("Location: index.php");
+    exit();
+}
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Login</title>
+  </head>
+
+  <body>
+
+    <h1>Login Form</h1>
+
+    <form action="/validate.php" method = "post">
+      <label for="username">Username:</label>
+      <br>
+      <input type="text" id="username" name="username">
+      <br>
+      <label for="password">Password:</label>
+      <br>
+      <input type="password" id="password" name="password">
+      <br><br>
+      <input type="submit" value="Submit">
+    </form>
+
+  </body>
+</html>
