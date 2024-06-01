d72d2c5aae6e46d3ccc72bb41f39af6a79ebfb6e
Ashvpatel <ashvpatel@algomau.ca>
2024-06-01 01:31:46 +0000

    Initial commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_HOST',         'b40.h.filess.io');
+define('DB_USER',         'cosc4806_petletcup'); 
+define('DB_PASS',         $_ENV['DB_PASS']);
+define('DB_DATABASE',     'cosc4806_petletcup');
+define('DB_PORT',         '3305');
+
+?>
