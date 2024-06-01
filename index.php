6109d360141fbe314228a8d9e42942957f33133c
Ashvpatel <ashvpatel@algomau.ca>
2024-06-01 03:07:47 +0000

    Updated and added welcome message and session variable respectively. and added link to log out, log in and register

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
-
 <?php
-
+session_start();
 require_once ('user.php');
 
 $user = new User();
 $user_list = $user->get_all_users();
 
+if (isset($_SESSION['username'])) {
+    echo "<h1>Welcome, " . htmlspecialchars($_SESSION['username']) . "!</h1>";
+    echo '<p><a href="logout.php">Logout</a></p>';
+} else {
+    echo '<p><a href="register.php">Register</a></p>';
+    echo '<p><a href="login.php">Login</a></p>';
+}
+
+
 echo "<pre>";
 print_r ($user_list);
+
+?>
